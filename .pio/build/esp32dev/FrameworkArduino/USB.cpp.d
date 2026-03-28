48ebe702ae1e1772d633678e92a63f0febd4a97c
zekageri <drsedate@gmail.com>
2026-03-28 21:46:55 +0100

    Core hardening

--- .pio/build/esp32dev/FrameworkArduino/USB.cpp.d
+++
@@ -1,5 +0,0 @@
-.pio/build/esp32dev/FrameworkArduino/USB.cpp.o: \
- /home/zekageri/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.cpp \
- /home/zekageri/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.h \
- /home/zekageri/.platformio/packages/framework-arduinoespressif32-libs/esp32/include/soc/esp32/include/soc/soc_caps.h \
- /home/zekageri/.platformio/packages/framework-arduinoespressif32-libs/esp32/dio_qspi/include/sdkconfig.h
--- .pio/build/esp32dev/FrameworkArduino/firmware_msc_fat.c.d
+++
@@ -1,4 +0,0 @@
-.pio/build/esp32dev/FrameworkArduino/firmware_msc_fat.c.o: \
- /home/zekageri/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.c \
- /home/zekageri/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.h \
- /home/zekageri/.platformio/packages/framework-arduinoespressif32-libs/esp32/include/newlib/platform_include/stdio.h
